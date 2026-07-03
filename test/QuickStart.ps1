761e8a3cdbc90b0f161b4c7cd1fcd16e2294d643
KOBAYASHI <1731237095@qq.com>
2026-07-03 23:09:55 +0800

    2026-07-03-23-09-38

--- test/QuickStart.ps1
+++ test/QuickStart.ps1
@@ -1,5 +1,4 @@
 ﻿$commands = @(
-    "sunshine"
     "D:\Program\Code\SimpleFileServer\start.ps1"
     "D:\Program\Code\Visual_Novel_Database\start-prod.ps1"
     "D:\Temp\frpc-win10-win11\frpc.exe -c D:\Temp\frpc-win10-win11\frpc.toml"
