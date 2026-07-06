9f85a40be743d32e26bbcfe8cac60a4787274af8
KOBAYASHI <1731237095@qq.com>
2026-07-06 08:29:35 +0800

    2026-07-06-08-29-28

--- test/QuickStart.ps1
+++ test/QuickStart.ps1
@@ -1,7 +1,6 @@
 ﻿$commands = @(
     "D:\Program\Code\SimpleFileServer\start.ps1"
     "D:\Program\Code\Visual_Novel_Database\start-prod.ps1"
-    "D:\Temp\frpc-win10-win11\frpc.exe -c D:\Temp\frpc-win10-win11\frpc.toml"
 )
 
 run-cmds-in-wt @commands -NoProfile
